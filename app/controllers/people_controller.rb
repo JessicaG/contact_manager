bf9a4d3b3b3004005c76b5914a54b43b4c58ad44
Jessica Goulding <jgoulding2@gmail.com>
2014-09-23 14:54:09 -0600

    implementing validations on person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,28 +1,24 @@
 class PeopleController < ApplicationController
   before_action :set_person, only: [:show, :edit, :update, :destroy]
 
-  # GET /people
-  # GET /people.json
   def index
     @people = Person.all
   end
 
-  # GET /people/1
-  # GET /people/1.json
   def show
   end
 
-  # GET /people/new
   def new
     @person = Person.new
   end
 
-  # GET /people/1/edit
   def edit
   end
 
-  # POST /people
-  # POST /people.json
+  def valid_attributes
+    { first_name: "John", last_name: "Doe" }
+  end
+
   def create
     @person = Person.new(person_params)
 
@@ -37,8 +33,6 @@ class PeopleController < ApplicationController
     end
   end
 
-  # PATCH/PUT /people/1
-  # PATCH/PUT /people/1.json
   def update
     respond_to do |format|
       if @person.update(person_params)
@@ -51,8 +45,6 @@ class PeopleController < ApplicationController
     end
   end
 
-  # DELETE /people/1
-  # DELETE /people/1.json
   def destroy
     @person.destroy
     respond_to do |format|
